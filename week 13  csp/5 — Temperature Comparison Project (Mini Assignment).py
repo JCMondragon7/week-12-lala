42d8762bb8d91706bfa3437b5a836891402cfe46
JCMondragon7 <johnathan12907@gmail.com>
2025-12-01 15:01:27 +0000

    temp comp stuf

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -12,3 +12,16 @@
 
 # Starter Code:
 
+temp = int(input("whats the temp outside?"))
+#if the temp is between 70 to 150 it is hot
+#if its 50 to 69 you its warm
+# -20 to 49 cold
+
+if temp >= 70 and temp <= 150:
+    print("thats really toatsy")
+
+elif temp >= 50 and temp <=59:
+    print("thats decently warm")
+
+elif temp >= 0 and temp <= 49:
+    print("yikes thats cold")
